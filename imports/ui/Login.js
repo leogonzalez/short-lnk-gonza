032e9702b4d557ffdd409c6b76afc7961508f95f
Leo Gonzalez <leo.lujan.gonzalez@gmail.com>
2017-06-23 18:24:10 -0300

    Vai caraio

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -41,7 +41,7 @@ export default class Login extends React.Component {
         noValidate>
           <input type='email' ref='email' name='email' placeholder="Email"/>
           <input type='password' ref='password' name='password' placeholder='Password'/>
-          <button className='button'>Login</button>
+          <button className='button'>Login Vai Caraio</button>
         </form>
 
         <Link to='/signup'>Don't have an account? </Link>
